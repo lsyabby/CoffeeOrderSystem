c8ece87e96f3e9d724066c12ee171894c8b91b33
lsyabby <amyandlisa21022@gmail.com>
2018-06-18 11:10:06 +0800

    added 8 btns

---
+++ CoffeeOrderSystem/Controller/ItemList/ItemBtnViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ItemBtnViewController.swift
+//  CoffeeOrderSystem
+//
+//  Created by 李思瑩 on 2018/6/18.
+//  Copyright © 2018年 李思瑩. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ItemBtnViewController: UIViewController {
+
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ CoffeeOrderSystem/Controller/ItemList/ItemListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ItemListViewController.swift
+//  CoffeeOrderSystem
+//
+//  Created by 李思瑩 on 2018/6/18.
+//  Copyright © 2018年 李思瑩. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+    
+    
+    var itemList: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
+
+extension ItemListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+}
